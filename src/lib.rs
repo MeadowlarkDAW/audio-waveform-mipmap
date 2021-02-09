938dd1ea14c7071852618c158f9ee1505a07b707
Roope Salmi <rpsalmi@gmail.com>
2021-02-09 02:08:07 +0200

    query_exact

--- src/lib.rs
+++ src/lib.rs
@@ -110,8 +110,8 @@ impl SampleMipMap {
         
         let padding = width / (pixels as f64);
         let mut interval = (
-            ((left - padding).ceil() as usize).max(0).min(self.size - 1),
-            ((left + width + padding).floor() as usize).max(0).min(self.size - 1),
+            (((left - padding).ceil() as i64).max(0) as usize).min(self.size - 1),
+            (((left + width + padding).floor() as i64).max(0) as usize).min(self.size - 1),
         );
         let mut layer = 0;
         let mut layer_piece_size = 1f64;
@@ -126,6 +126,7 @@ impl SampleMipMap {
             let x = (i as f64 + 0.5) * layer_piece_size;
             (x - left) / width * (pixels as f64)
         };
+        let offset = self.layers[layer];
         (
             (layer_index_to_px(interval.0), layer_index_to_px(interval.1)),
             // RangeInclusive<usize>: !ExactSizeIterator :(
@@ -134,10 +135,69 @@ impl SampleMipMap {
                     let v = self.config.convert_sample(data[i]);
                     (v, v)
                 } else {
-                    self.tree[self.layers[layer] + i]
+                    self.tree[offset + i]
                 }
             }),
         )
+    }
+    
+    /// Query a range of data exactly.
+    ///
+    /// Produces exactly `pixels + 2` points unless querying out of bounds.
+    pub fn query_exact<'a>(&'a self, data: &'a [f32], left: f64, width: f64, pixels: usize)
+        -> ((f64, f64), impl ExactSizeIterator<Item = (i8, i8)> + 'a) {
+        assert!(data.len() == self.size);
+        
+        let samples_per_px = width / pixels as f64;
+        let max_px = (self.size as f64 / samples_per_px) as i64;
+        let left_px = (left / samples_per_px) as i64;
+        let interval = (
+            (left_px - 1).max(0).min(max_px),
+            (left_px + pixels as i64 + 1).max(0).min(max_px),
+        );
+        (
+            ((interval.0 - left_px) as f64 + 0.5, (interval.1 - left_px) as f64 + 0.5),
+            (interval.0 as usize..interval.1 as usize).map(move |i| {
+                self.range_min_max(data, (i as f64 * samples_per_px) as usize, ((i+1) as f64 * samples_per_px) as usize)
+            }),
+        )
+    }
+    
+    /// Returns minimum and maximum in the sample range [left, right).
+    ///
+    /// O(BASE * log_BASE(right - left))
+    fn range_min_max(&self, data: &[f32], mut left: usize, mut right: usize) -> (i8, i8) {
+        left = max(left, 0);
+        right = min(right, self.size);
+        
+        let mut result = (std::i8::MAX, std::i8::MIN);
+        while left < right && left % BASE != 0 {
+            let v = self.config.convert_sample(data[left]);
+            result = (min(result.0, v), max(result.1, v));
+            left += 1;
+        }
+        while left < right && right % BASE != 0 {
+            right -= 1;
+            let v = self.config.convert_sample(data[right]);
+            result = (min(result.0, v), max(result.1, v));
+        }
+        let mut layer_iter = self.layers.iter().skip(1);
+        while left < right {
+            left /= BASE;
+            right /= BASE;
+            let layer = &self.tree[*layer_iter.next().unwrap()..];
+            while left < right && left % BASE != 0 {
+                let p = layer[left];
+                result = (min(result.0, p.0), max(result.1, p.1));
+                left += 1;
+            }
+            while left < right && right % BASE != 0 {
+                right -= 1;
+                let p = layer[right];
+                result = (min(result.0, p.0), max(result.1, p.1));
+            }
+        }
+        result
     }
 }
 
@@ -166,18 +226,36 @@ mod tests {
     #[test]
     fn small_query() {
         let s = make_small();
-        let (a, b) = s.query(&SMALL_DATA[..], 2., 3., 3);
+        let ((l, r), b) = s.query(&SMALL_DATA[..], 2., 3., 3);
         let v: Vec<_> = b.collect();
 
-        assert!(a.0 - (-0.5) <= EPS);
-        assert!(a.1 - 3.5 <= EPS);
+        assert!(l - (-0.5) <= EPS);
+        assert!(r - 3.5 <= EPS);
         assert_eq!(v, [(127, 127), (-127, -127), (0, 0), (64, 64), (-64, -64)]);
 
-        let (a, b) = s.query(&SMALL_DATA[..], 0., 6., 1);
+        let ((l, r), b) = s.query(&SMALL_DATA[..], 0., 6., 1);
         let v: Vec<_> = b.collect();
 
-        assert!(a.0 - 0.33333 <= EPS);
-        assert!(a.1 - 1.0 <= EPS);
+        assert!(l - 0.33333 <= EPS);
+        assert!(r - 1.0 <= EPS);
+        assert_eq!(v, [(-127, 127), (-64, 64)]);
+    }
+    
+    #[test]
+    fn small_query_exact() {
+        let s = make_small();
+        let ((l, r), b) = s.query_exact(&SMALL_DATA[..], 2., 3., 3);
+        let v: Vec<_> = b.collect();
+
+        assert!(l - (-0.5) <= EPS);
+        assert!(r - 4.5 <= EPS);
+        assert_eq!(v, [(127, 127), (-127, -127), (0, 0), (64, 64), (-64, -64)]);
+        
+        let ((l, r), b) = s.query_exact(&SMALL_DATA[..], 0., 6., 2);
+        let v: Vec<_> = b.collect();
+
+        assert!(l - 0.5 <= EPS);
+        assert!(r - 2.5 <= EPS);
         assert_eq!(v, [(-127, 127), (-64, 64)]);
     }
 }
